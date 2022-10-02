0702fa81eaf36b34dc184dde5cdf15ae874ddd46
Jerry Liu <jl6007@columbia.edu>
2022-10-02 20:55:16 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,18 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    return ['G','PG','PG-13','R']
+  end
+
+  def self.with_ratings(ratings_list)
+    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+    #  movies with those ratings
+    # if ratings_list is nil, retrieve ALL movies
+    if ratings_list.empty?
+      return Movie.all
+    else
+      ratings_upper = ratings_list.map(&:upcase)
+      return Movie.where(rating: ratings_upper)
+    end
+  end
+
 end
